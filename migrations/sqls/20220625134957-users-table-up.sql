0a9c8bfe76757cbbd8a013934c7c62e76debbbc0
alsabagh-dev <zizo.naser@stud.fci-cu.edu.eg>
2022-06-25 19:18:19 +0200

    fix: make username unique

--- migrations/sqls/20220625134957-users-table-up.sql
+++ migrations/sqls/20220625134957-users-table-up.sql
@@ -2,6 +2,6 @@ CREATE TABLE users (
         id SERIAL PRIMARY KEY,
         firstName VARCHAR(100) NOT NULL,
         lastName VARCHAR(100) NOT NULL,
-        userNAme VARCHAR(100) NOT NULL,
+        userNAme VARCHAR(100) NOT NULL UNIQUE,
         password VARCHAR NOT NULL
 );
