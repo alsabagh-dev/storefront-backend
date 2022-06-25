90573cc8ba845cebf5901f3c5131affb1193bb92
alsabagh-dev <zizo.naser@stud.fci-cu.edu.eg>
2022-06-26 01:22:49 +0200

    feat: add user controller

---
+++ src/controller/user.controller.ts
@@ -0,0 +1,65 @@
+import { Request, Response } from 'express';
+import { User, UserStore } from '../models/user.model';
+
+const store = new UserStore();
+
+export default class UserController {
+    async getUsers(req: Request, res: Response): Promise<void> {
+        try {
+            const users = await store.index()
+            res.json(users);
+        } catch (error) {
+            console.error(error);
+
+            res.status(500);
+            res.send(`Sorry can not get users, please try again later`);
+        }
+    }
+
+    async get_user(req: Request, res: Response): Promise<void> {
+        const user_id = req.params.id;
+        try {
+            const user = await store.show(user_id);
+            res.json(user);
+        } catch (error) {
+            console.error(error);
+            res.status(404);
+            res.send(`Sorry can not find user ${user_id}`);
+        }
+    }
+
+    // @TODO return token
+    async add_user(req: Request, res: Response): Promise<void> {
+        const {
+            first_name,
+            last_name,
+            user_name,
+            password
+        } = req.body;
+
+        if (first_name === undefined || user_name === undefined
+            || last_name === undefined || password === undefined) {
+            res.status(400);
+            res.send(`missing field(s) user must have 'first_name', 'last_name',
+             'user_name', and password`);
+        } else {
+            const new_user: User = {
+                first_name,
+                last_name,
+                user_name,
+                password
+            }
+            try {
+                const user = await store.create(new_user);
+                res.json(user);
+            } catch (error) {
+                console.error(error);
+
+                res.status(500);
+                res.send(`Sorry can not create users, please try again later`);
+            }
+        }
+    }
+
+     // @Todo : update and delete. (done in model. )
+}
