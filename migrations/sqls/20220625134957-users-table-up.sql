06f439d60a13a894aed40e839d25959c4fdfa014
alsabagh-dev <zizo.naser@stud.fci-cu.edu.eg>
2022-06-25 16:03:30 +0200

    fix: extra comma

--- migrations/sqls/20220625134957-users-table-up.sql
+++ migrations/sqls/20220625134957-users-table-up.sql
@@ -2,5 +2,5 @@ CREATE TABLE users (
         id SERIAL PRIMARY KEY,
         firstName VARCHAR(100) NOT NULL,
         lastName VARCHAR(100) NOT NULL,
-        password TEXT NOT NULL,
+        password TEXT NOT NULL
 );
