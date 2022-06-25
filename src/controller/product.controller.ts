81bfc6f6842854f9b1551cec3db3d1c71c66a761
alsabagh-dev <zizo.naser@stud.fci-cu.edu.eg>
2022-06-26 01:13:00 +0200

    feat: add product controller

---
+++ src/controller/product.controller.ts
@@ -0,0 +1,53 @@
+import { Product, ProductStore } from '../models/product.model';
+import { Express, Response, Request } from 'express';
+const store = new ProductStore();
+
+export default class ProductController {
+    async get_products(_req: Request, res: Response): Promise<void> {
+        try {
+            const products = await store.index()
+            res.json(products);
+        } catch (error) {
+            console.error(error);
+
+            res.status(500);
+            res.send(`Sorry can not get products, please try again later`);
+        }
+    }
+
+    async get_product(req: Request, res: Response): Promise<void> {
+        const product_id = req.params.id;
+        try {
+            const product = await store.show(product_id);
+            res.json(product);
+        } catch (error) {
+            console.error(error);
+            res.status(404);
+            res.send(`Sorry can not find product ${product_id}`);
+        }
+    }
+
+    async add_product(req: Request, res: Response): Promise<void> {
+        const name = req.body.name, price = parseFloat(req.body.price);
+        if (name === undefined || price === undefined) {
+            res.status(400);
+            res.send(`missing field(s) product must have 'name' and 'price'`)
+        } else {
+            const new_product: Product = {
+                name: name,
+                price: price
+            } 
+            try {
+                const product = await store.create(new_product);
+                res.json(product);
+            } catch (error) {
+                console.error(error);
+
+                res.status(500);
+                res.send(`Sorry can not create products, please try again later`);
+            }
+        }
+    }
+
+    // @Todo : update and delete. (done in model. )
+}
