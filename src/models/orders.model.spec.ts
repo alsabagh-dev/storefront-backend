01b82fb676ed9921de1eaed547fca9b50314220b
alsabagh-dev <zizo.naser@stud.fci-cu.edu.eg>
2022-06-26 00:44:32 +0200

    feat: add order model

---
+++ src/models/orders.model.spec.ts
@@ -0,0 +1,106 @@
+import { Order, OrderStore } from './orders.model';
+import { User, UserStore } from './user.model';
+import { Product, ProductStore } from './product.model';
+
+const store = new OrderStore();
+const userStore = new UserStore();
+const productStore = new ProductStore();
+
+const users: User[] = [];
+const products: Product[] = [];
+const quantites: number[] = [];
+const orders: Order[] =  [];
+
+describe('Order Model', () => {
+
+    beforeAll(async () => {
+        // create some products
+        for (let i = 1; i < 10; i++) {
+            const product = await productStore.create({
+                name: `product0.0.${i}-order`,
+                price: i
+            });
+
+            products.push(
+                product
+            );
+            quantites.push(i);
+        }
+        // create some users and orders
+        for (let ID = 1; ID < 10; ID++) {
+            const user: User = {
+                first_name: `first0.0.${ID}-order`,
+                last_name: `last0.0.${ID}-order`,
+                user_name: `user0.0.${ID}-order`,
+                password: `pass0.0${ID}-order`,
+            }
+            const created_user = await userStore.create(user);    
+            user.id = created_user.id;
+            users.push(
+                user
+            )
+            orders.push({
+                user_id: user.id?.toString() as string,
+                products: [...products],
+                quantites: [...quantites],
+                status: false
+            })
+        }
+    })
+
+    it('should have an index method', () => {
+        expect(store.index).toBeDefined();
+    });
+
+    it('should have a show method', () => {
+        expect(store.show).toBeDefined();
+    });
+
+    it('should have a create method', () => {
+        expect(store.create).toBeDefined();
+    });
+
+    it('should have a edit method', () => {
+        expect(store.edit).toBeDefined();
+    });
+
+    it('should have a delete method', () => {
+        expect(store.delete).toBeDefined();
+    });
+
+    it('create method should add an order', async () => {
+        const result = await store.create(users[0], orders[0]);
+        expect(result.user_id).toEqual(''+users[0].id?.toString());
+    });
+
+    it('index method should return a list of orders', async () => {
+        await store.create(users[1], orders[1]);
+        const result = await store.index();
+        expect(result.length).toBeGreaterThan(0);
+    });
+
+    it('show method should return the correct product', async () => {
+        const insert_result = await store.create(users[2], orders[2]);
+        const select_result = await store.show('' + insert_result.id);
+        expect(select_result?.user_id as string).toEqual(''+users[2].id?.toString());
+        expect(select_result?.products.length as number).toEqual(orders[2].products.length);
+        
+    });
+
+    it('edit method should update the correct product', async () => {
+        const insert_result = await store.create(users[5], orders[5]);
+        insert_result.status = true;
+        insert_result.products = [];
+
+        const updated_order = await store.edit(insert_result);
+        expect(updated_order.status).toBeTrue();
+        expect(updated_order.products.length).toEqual(0);
+    });
+
+    it('delete method should remove the order', async () => {
+        const insert_result = await store.create(users[4], orders[4]);
+        await store.delete('' + insert_result.id);
+        const showed = await store.show('' + insert_result.id);
+        expect(showed).toBeNull();
+    });
+});
---
+++ src/models/orders.model.ts
@@ -0,0 +1,200 @@
+import client from '../database/pool.db';
+import { Product, ProductStore } from './product.model';
+import { User } from './user.model';
+
+
+
+export type Order = {
+    id?: number;
+    status: boolean;  // false = active, true == completed
+    user_id: string;
+    products: Product[];
+    quantites: number[];
+}
+
+export class OrderStore {
+    async index(): Promise<Order[]> {
+        try {
+            const orders: Order[] = [];
+            const productStore = new ProductStore();
+            // get connection
+            const connection = await client.connect();
+            // Get orders
+            // setup query
+            let query = 'Select * from orders';
+            // excute the query
+            const rows = (await connection.query(query)).rows;
+            for (const row of rows) {
+                const order: Order = {
+                    id: row.id,
+                    user_id: row.user_id,
+                    status: row.status,
+                    products: [],
+                    quantites: []
+                }
+                // Get Products and quantites
+                query = `Select quantity, product_id from orders inner join order_products
+                on orders.id = order_products.order_id where order_id = $1 ;`;
+                const results = (await connection.query(query, [order.id])).rows;
+                for (const result of results) {
+                    order.quantites.push(parseInt(result.quantity));
+                    order.products.push(await productStore.show(result.product_id));
+                }
+                orders.push(order);
+            }
+
+            // end connection
+            connection.release();
+
+            return orders;
+        } catch (error) {
+            throw new Error(`Can not get orders. Error: ${error}`);
+        }
+    }
+
+    async show(id: string): Promise<Order | null> {
+        try {
+            const productStore = new ProductStore();
+            // get connection
+            const connection = await client.connect();
+            // Get orders
+            // setup query
+            let query = 'Select * from orders where id = $1';
+            // excute the query
+            const result_order = (await connection.query(query, [id])).rows[0];
+            if (! result_order) return null;
+
+            const order: Order = {
+                id: result_order.id,
+                user_id: result_order.user_id,
+                status: result_order.status,
+                products: [],
+                quantites: []
+            }
+            // Get Products and quantites
+            query = `Select * from orders inner join order_products
+            on orders.id = order_products.order_id where order_id = $1`;
+            const results = (await connection.query(query, [order.id])).rows;
+            for (const result of results) {
+                order.quantites.push(parseInt(result.quantity));
+                order.products.push(await productStore.show(result.product_id));
+            }
+
+            // end connection
+            connection.release();
+
+            return order;
+        } catch (error) {
+            throw new Error(`can not get order ${id}. Error: ${error}`);
+        }
+    }
+
+    async create(user: User, order: Order): Promise<Order> {
+        try {
+            const productStore = new ProductStore();
+            // get connection
+            const connection = await client.connect();
+            // Create an order with user.id
+            // setup query
+            let query = 'INSERT INTO orders (status, user_id) VALUES ($1, $2) returning *';
+            // excute the query
+            const result_order = await (await connection.query(query, [order.status, user.id])).rows[0];
+            const new_order: Order = {
+                id: result_order.id,
+                user_id: result_order.user_id,
+                status: result_order.status,
+                products: [],
+                quantites: []
+            }
+
+            // Add products to the order
+            query = `INSERT INTO order_products(
+                quantity, order_id, product_id) 
+                VALUES ($1, $2, $3) returning * `;
+            for (const idx in order.products) {
+                    const product = order.products[idx];
+                    const quantity = order.quantites[idx];
+                    const row = (await connection.query(query,
+                        [quantity, result_order.id, product.id])).rows[0];
+                    new_order.products.push(await productStore.show(row.product_id));
+                    new_order.products.push(row.quantity);
+                
+            }
+
+            // end connection
+            connection.release();
+
+            return new_order;
+        } catch (error) {
+            throw new Error(`can not create new order for user ${user.user_name}. Error: ${error}`);
+        }
+    }
+
+    async edit(order: Order): Promise<Order> {
+        // can not change order user_id
+        // will change status and replace the old products and quantites
+        // it shlould be a transaction
+        try {
+            const productStore = new ProductStore();
+            // get connection
+            const connection = await client.connect();
+            // Remove old products and quantites
+            let query = 'DELETE FROM order_products WHERE order_id=$1';
+            // excute the query
+            await connection.query(query, [order.id]);
+            // update
+            // setup query
+            query = 'UPDATE orders SET status = $1 WHERE id=$2 returning *';
+            // excute the query
+            const result_order = await (await connection.query(query, [order.status, order.id])).rows[0];
+            const new_order: Order = {
+                id: result_order.id,
+                user_id: result_order.user_id,
+                status: result_order.status,
+                products: [],
+                quantites: []
+            }
+            // Add products to the order
+            query = `INSERT INTO order_products(
+                quantity, order_id, product_id) 
+                VALUES ($1, $2, $3) returning * `;
+            for (const idx in order.products) {
+                    const product = order.products[idx];
+                    const quantity = order.quantites[idx];
+                    const row = (await connection.query(query,
+                        [quantity, result_order.id, product.id])).rows[0];
+                    new_order.products.push(await productStore.show(row.product_id));
+                    new_order.products.push(row.quantity);
+                
+            }
+            // end connection
+            connection.release();
+
+            return new_order;
+        } catch (error) {
+            throw new Error(`can not update order ${order.id}. Error: ${error}`);
+        }
+    }
+
+    async delete(id: string): Promise<Boolean> {
+        try {
+            // get connection
+            const connection = await client.connect();
+            // setup query
+            let query = 'DELETE FROM order_products WHERE order_id=($1)';
+            // excute the query
+            await connection.query(query, [id]);
+            // setup 2nd query
+            query = 'DELETE FROM orders WHERE id=($1)';
+            // excute 2nd query
+            await connection.query(query, [id]);
+            // end connection
+            connection.release();
+
+            return true;
+        } catch (error) {
+            throw new Error(`can not delete order ${id}. Error: ${error}`);
+        }
+    }
+
+}
